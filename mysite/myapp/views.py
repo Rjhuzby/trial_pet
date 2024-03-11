2f9bd2f2a5fa5918526eebeae3590b21ec825875
Rjhuzby <klarcky@yandex.ru>
2024-03-11 18:25:46 +0300

    create add and delete item

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 
 
@@ -16,3 +16,34 @@ def indexItem(request, my_id):
         'item': item,
     }
     return render(request, 'myapp/detail.html', context=context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+        image = request.FILES.get('upload') 
+        item = Product(name=name, price=price, description=description, image=image).save()
+    return render(request, 'myapp/additem.html')
+
+def update_item(request, my_id):
+    item = Product.objects.get(id=my_id)
+    if request.method == 'POST':
+        item.name = request.POST.get('name')
+        item.price = request.POST.get('price')
+        item.description = request.POST.get('description')
+        item.image = request.FILES.get('upload', item.image) 
+        item.save()
+        return redirect('/myapp')
+    context = {'item': item,}
+    return render(request, 'myapp/updateitem.html', context)
+
+
+def delete_item(request, my_id):
+    item = Product.objects.get(id=my_id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/myapp')
+    context = {'item': item,}
+    return render(request, 'myapp/delete.html', context)
